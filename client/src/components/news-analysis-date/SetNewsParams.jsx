51a32fe7371fa67168cee7187b6f0013cab92506
ArturN31 <91878136+ArturN31@users.noreply.github.com>
2024-04-28 21:00:57 +0100

    DatePicker. Form - on submit get year, month, and count.

---
+++ client/src/components/news-analysis-date/SetNewsParams.jsx
@@ -0,0 +1,78 @@
+import { useState } from "react";
+import { Row, Col, Form, Button } from "react-bootstrap";
+
+//import NewsFetch from "./NewsFetch";
+import DatePicker from "react-date-picker";
+
+function SetNewsParams() {
+    const [params, setParams] = useState({
+        year: 2023,
+        month: 1,
+        count: 1,
+    });
+
+    const [date, setDate] = useState(new Date());
+
+    const handleDateChange = (date) => {
+        setParams({ ...params, year: date.getFullYear(), month: date.getMonth() + 1 });
+    };
+
+    const handleCountChange = (e) => {
+        setParams({ ...params, count: e.target.value });
+    };
+
+    const onChange = (date) => {
+        setDate(date);
+        handleDateChange(date);
+    }
+
+    const handleSubmit = async () => {
+        console.log(params)
+    };
+
+    return (
+        <>
+            <Row>
+                <Col>
+                    <Form className="col-6 mx-auto user-input-div text-center">
+                        <Row className="text-center">
+                            <Col>
+                                <p>First choose the date on the calendar presented bellow, then press submit and enter the article count to be displayed.</p>
+                            </Col>
+                        </Row>
+                        <Form.Group controlId="f_themes">
+                            <DatePicker
+                                onChange={onChange}
+                                value={date}
+                                maxDetail='year'
+                                minDate={new Date("01-01-1852")}
+                                maxDate={new Date()}
+                                className='mx-auto' />
+                            <p className="mt-3">Chosen date: {date.toDateString().split(' ')[1]} {date.toDateString().split(' ')[3]}</p>
+                        </Form.Group>
+                        <Button className="m-4 mt-2 btn-submit" onClick={() => { handleSubmit() }}>
+                            Submit
+                        </Button>
+                        <Form.Group controlId="f_count">
+                            <Form.Label>Article Count:</Form.Label>
+                            <Form.Control
+                                type="number"
+                                onChange={handleCountChange}
+                                min='0'
+                                max='20'
+                                className="w-50 mx-auto"
+                            />
+                        </Form.Group>
+                    </Form>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    {/* <NewsFetch count={params.count} /> */}
+                </Col>
+            </Row>
+        </>
+    );
+}
+
+export default SetNewsParams;
--- client/src/pages/NewsAnalysisByDate.jsx
+++ client/src/pages/NewsAnalysisByDate.jsx
@@ -1,12 +1,12 @@
 import { Container } from 'react-bootstrap';
 
-//import SetNewsParams from '../components/news-analysis-date/SetNewsParams';
+import SetNewsParams from '../components/news-analysis-date/SetNewsParams';
 
 const NewsAnalysisByDate = () => {
     return (
         <Container fluid>
             <h1 className='text-center m-5 mb-3 text-white'>Analysis by date</h1>
-            {/* <SetNewsParams /> */}
+            <SetNewsParams />
         </Container>
     );
 }
