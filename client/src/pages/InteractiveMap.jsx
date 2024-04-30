0e5662bd9579a02a474a56e0bd709819d0315559
ArturN31 <91878136+ArturN31@users.noreply.github.com>
2024-04-30 20:23:22 +0100

    Interactive Map - fetch by date

--- client/src/pages/InteractiveMap.jsx
+++ client/src/pages/InteractiveMap.jsx
@@ -1,9 +1,11 @@
 import { Container } from 'react-bootstrap';
+import SetNewsParams from '../components/news-analysis-date/SetNewsParams'
 
 function InteractiveMap() {
     return (
         <Container fluid>
             <h1 className='text-center m-5 mb-3 text-white'>Interactive Map</h1>
+            <SetNewsParams />
         </Container>
     );
 }
